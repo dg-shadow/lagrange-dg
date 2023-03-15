2ad8833164695c6db0574e60db504857053d0136
dg-shadow <dg@shadowrobot.com>
2023-03-15 08:40:01 +0100

    started working

--- first_well.py
+++ first_well.py
@@ -3,52 +3,73 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from copy import deepcopy
+from time import process_time
+
 
 G=6.6743e-11
 
 class GravitationalMass(object):
-    def __init__(self, mass, location, name=None):
+    def __init__(self, mass, location, radius, name=None):
         self._mass = mass
-        self._location = np.asarray(location)
+        self._x = location[0]
+        self._y = location[1]
         self._name = name
+        self._radius = radius
+        self._r2 = radius**2
 
-    def field_contribution(self, location):
-        r = location - self._location  # Vector of distance
-        r2 = numpy.square(r).sum()
+    def scalar_field_contribution(self, x, y):
+        x -= self._x
+        y -= self._y
+
+        r2 = y*y + x*x
+
+        if r2 <= self._r2:
+            return 0
+
         field = -G*self._mass/r2
         return field
 
+    # def get_scalar_field(self, xx, yy):
+    #     area = np.dstack((xx, yy))
+    #     return np.apply_along_axis(self.scalar_field_contribution, 2, area)
+
+    # d = area - self._location
+    # magnitudes = np.apply_along_axis(np.linalg.norm,  2, d)
+    # units = d / magnitudes
 
 
 
-earth = GravitationalMass(5.972e24, [0,0])
 
+earth = GravitationalMass(5.972e24, [0,       0], 6.371e6)
+moon  = GravitationalMass(7.348e22, [3.844e8, 0], 1.737e6)
 
-x = np.arange(-1e8, 5e8, 10e5)
-y = np.arange(-1e8, 1e8, 10e5)
+x = np.arange(-4e8, 10e8, 10e5)
+y = np.arange(-4e8, 4e8, 10e5)
 
 xv, yv = np.meshgrid(x, y)
 
-
+start = process_time()
 h = np.empty((y.shape[0], x.shape[0])) #-1/((xv**2 + yv**2)+1e-10)
-print (h.shape)
-
 
 for i, x_ in enumerate(x):
     for j, y_ in enumerate(y):
-        h[j][i]  = -1/((x_**2 + y_**2)+1e-10)
+        h[j][i]  = earth.scalar_field_contribution(x_, y_) + moon.scalar_field_contribution(x_,y_)
+
+print (process_time() - start)
+
+# start = process_time()
+# h2 = earth.get_scalar_field(xv, yv)
+# print (process_time() - start)
 
 
 
-exit()
+#exit()
 
 
 print (np.min(h))
 print (np.max(h))
 
-lev=np.linspace(-1e15, 0, 500)
-
-lev = [(i - 500) * 1e-15 for i in range(501)]
+lev=np.linspace(-0.02, 0, 500)
 
 
 
@@ -58,6 +79,6 @@ cs = plt.contour(x, y, h, levels=lev) #, levels=[-50, -25, 0, 25, 50],
 #cs.cmap.set_over('red')
 #cs.cmap.set_under('blue')
 #cs.changed()
-
+#cs.add
 
 plt.show()
